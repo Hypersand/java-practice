6d3cc2a84214c54e174c2d652218b0aa87745446
Hypersand <on8214@gmail.com>
2023-07-12 16:47:59 +0900

    feat: 요구사항 2 완료

--- src/test/java/next/reflection/Junit3Runner.java
+++ src/test/java/next/reflection/Junit3Runner.java
@@ -1,11 +1,28 @@
 package next.reflection;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class Junit3Runner {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
     @Test
     public void runner() throws Exception {
         Class clazz = Junit3Test.class;
+
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.getName().startsWith("test")) {
+                logger.debug("Classs method {}", method.getName());
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
+
 
     }
 }
